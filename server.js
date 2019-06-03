6b4e98e0ffd9cddafc97182b9385e230915f62be
amlane <twelvexstring@gmail.com>
2019-06-03 15:27:45 -0500

    NPM not working had to switch to yarn and created get end point

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 
+const db = require('./data/accounts-model.js');
+
 const server = express();
 
+server.use(express.json());
+
 // your code here
+
+server.get('/', (req, res) => {
+    db.find()
+    .then( data => {
+        res.status(200).json(data)
+    })
+    .catch(error => {
+        res.status(500).json({ error: "That's an error." })
+    })
+})
+
+
 
 module.exports = server;
