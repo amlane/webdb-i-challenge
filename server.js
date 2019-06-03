0ab4e947c0adeccdbf82a8956e2d56d360377be1
amlane <twelvexstring@gmail.com>
2019-06-03 15:50:07 -0500

    created all CRUD end points and tested in postman

--- server.js
+++ server.js
@@ -14,10 +14,55 @@ server.get('/', (req, res) => {
         res.status(200).json(data)
     })
     .catch(error => {
-        res.status(500).json({ error: "That's an error." })
+        res.status(500).json({ error: "That's an error getting." })
     })
 })
 
+server.get('/:id', (req, res) => {
+    const id = req.params.id;
 
+    db.findById(id)
+    .then( account => {
+        res.status(200).json(account)
+    })
+    .catch(error => {
+        res.status(500).json({ error: "That's an error getting by id." })
+    })
+})
+
+server.post('/', (req, res) => {
+    db.add(req.body)
+    .then( account => {
+        res.status(201).json(account)
+    })
+    .catch(error => {
+        res.status(500).json({ error: "That's an error posting." })
+    })
+})
+
+server.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+    .then( deleted => {
+        res.status(204).end();
+    })
+    .catch(error => {
+        res.status(500).json({ error: "That's an error posting." })
+    })
+})
+
+server.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    db.update(id, changes)
+    .then( updated => {
+        res.status(200).json(updated)
+    })
+    .catch(error => {
+        res.status(500).json({ error: "That's an error updating." })
+    })
+})
 
 module.exports = server;
